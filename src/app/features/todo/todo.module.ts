7df3bce7acaff1704c046a075dfb1ca82485666e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-08-26 15:01:11 +0000

    feat: update components UI

--- src/app/features/todo/todo.module.ts
+++ src/app/features/todo/todo.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 import { TodoComponent } from './todo.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoService } from './todo.service';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatInputModule,
+  ],
   declarations: [TodoComponent, TodoListComponent, TodoItemComponent],
   providers: [TodoService],
   exports: [TodoComponent],
